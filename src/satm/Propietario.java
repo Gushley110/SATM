f1f00276709a1ac6f64c6bf76a83f0b7f9a36fea
Gushley110 <gusley_dominator@hotmail.com>
2017-12-05 02:25:04 -0600

    Add Propietario class

---
+++ src/satm/Propietario.java
@@ -0,0 +1,41 @@
+package satm;
+
+public class Propietario {
+    
+    private int id;
+    private String nombre;
+    private int telefono;
+    
+    public Propietario(String nombre, int telefono){
+        
+        this.setNombre(nombre);
+        this.setTelefono(telefono);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
+}
