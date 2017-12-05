d1130689f9490483bc8399d6871aee1dcec3cce4
Gushley110 <gusley_dominator@hotmail.com>
2017-12-05 12:07:47 -0600

    Fix mysql credentials

--- src/bd/Conn.java
+++ src/bd/Conn.java
@@ -8,7 +8,7 @@ public class Conn {
         if (cnx == null) {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
-                cnx = DriverManager.getConnection("jdbc:mysql://localhost/java_mysql", "root", "");
+                cnx = DriverManager.getConnection("jdbc:mysql://localhost/satmdb", "root", "root");
             } catch (SQLException ex) {
                 throw new SQLException(ex);
             } catch (ClassNotFoundException ex) {
