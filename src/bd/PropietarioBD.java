a0764d4ec33147f3c3b5ec8c6fa0c16ad123f67a
Gushley110 <gusley_dominator@hotmail.com>
2017-12-06 11:04:28 -0600

    Add propietarioById method

--- src/bd/PropietarioBD.java
+++ src/bd/PropietarioBD.java
@@ -12,11 +12,22 @@ public class PropietarioBD {
     
     private final String tabla = "propietario";
     
-    public Propietario getPropietarioById(Connection c, int id){
+    public Propietario getPropietarioById(Connection c, int id) throws SQLException{
         
         Propietario p = null;
         
-        
+        try{
+            
+         PreparedStatement consulta = c.prepareStatement("SELECT * FROM " + this.tabla );
+         consulta.setInt(1, id);
+         ResultSet r = consulta.executeQuery();
+         
+         while(r.next()){
+             p = new Propietario(id,r.getString(2)+" "+r.getString(3)+" "+r.getString(4),r.getString(5));
+         }
+         }catch(SQLException ex){
+         throw new SQLException(ex);
+      }
         
         return p;
     }
