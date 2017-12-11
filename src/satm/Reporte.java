7ccff437ae4a138d7a15221002971f4c2e15e26c
Gushley110 <gusley_dominator@hotmail.com>
2017-12-11 13:57:40 -0600

    Change attributes and add getters and setters

--- src/satm/Reporte.java
+++ src/satm/Reporte.java
@@ -4,11 +4,22 @@ import java.sql.Date;
 
 public class Reporte {
     
+    private Integer id;
     private Date fechaEntrada;
     private Date fechaSalida;
-    private String descFalla;
-    private int pagoxserv;
-    private int pagoAdelanto;
+    private String tipoPago;
+    private Double pagoxserv;
+    private Double pagoAdelanto;
+    private Integer idUsuario;
+    private Integer idVehiculo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public Date getFechaEntrada() {
         return fechaEntrada;
@@ -25,29 +36,45 @@ public class Reporte {
     public void setFechaSalida(Date fechaSalida) {
         this.fechaSalida = fechaSalida;
     }
-    
-    public String getDescFalla() {
-        return descFalla;
+
+    public String getTipoPago() {
+        return tipoPago;
     }
 
-    public void setDescFalla(String descFalla) {
-        this.descFalla = descFalla;
+    public void setTipoPago(String tipoPago) {
+        this.tipoPago = tipoPago;
     }
 
-    public int getPagoxserv() {
+    public Double getPagoxserv() {
         return pagoxserv;
     }
 
-    public void setPagoxserv(int pagoxserv) {
+    public void setPagoxserv(Double pagoxserv) {
         this.pagoxserv = pagoxserv;
     }
 
-    public int getPagoAdelanto() {
+    public Double getPagoAdelanto() {
         return pagoAdelanto;
     }
 
-    public void setPagoAdelanto(int pagoAdelanto) {
+    public void setPagoAdelanto(Double pagoAdelanto) {
         this.pagoAdelanto = pagoAdelanto;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Integer getIdVehiculo() {
+        return idVehiculo;
+    }
+
+    public void setIdVehiculo(Integer idVehiculo) {
+        this.idVehiculo = idVehiculo;
     }
 
 }
